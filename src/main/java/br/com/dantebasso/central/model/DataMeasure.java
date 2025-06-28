59c415d2127ee644cc9687bc67011e18e804fad6
Dante Basso <dbasso@rccl.com>
2025-06-28 18:56:39 -0300

    Add skeleton of the project

---
+++ src/main/java/br/com/dantebasso/central/model/DataMeasure.java
@@ -0,0 +1,14 @@
+package br.com.dantebasso.central.model;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.UUID;
+
+public record DataMeasure(
+        UUID id,
+        String sensorId,
+        Integer sensorValue,
+        SensorType sensorType,
+        Map<String, Serializable> metadata
+) {
+}
