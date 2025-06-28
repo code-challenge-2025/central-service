37bd2283f2a8a3a5ccb941535a033af412774c6c
Dante Basso <dbasso@rccl.com>
2025-06-28 19:48:16 -0300

    addded topic consumer, without unit tests

---
+++ src/main/java/br/com/dantebasso/central/service/impl/DataMeasureConsumerServiceImpl.java
@@ -0,0 +1,21 @@
+package br.com.dantebasso.central.service.impl;
+
+import br.com.dantebasso.central.model.DataMeasure;
+import br.com.dantebasso.central.service.DataMeasureConsumerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class DataMeasureConsumerServiceImpl implements DataMeasureConsumerService {
+    
+    @Override
+    @KafkaListener(topics = "#{@warehouseKafkaConfig.topic}", groupId = "#{@warehouseKafkaConfig.groupId}")
+    public void consume(DataMeasure dataMeasure) {
+        log.info("Received message from Kafka: {}", dataMeasure);
+    }
+    
+}
